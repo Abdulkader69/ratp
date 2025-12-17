7d14cbdf38ed5ed5286d5c5232d843992a39fcc5
Abdul Kader <abdulkader.69k@gmail.com>
2025-12-18 01:48:57 +0600

    pages

--- main.js
+++ main.js
@@ -1,31 +1,5 @@
 (function ($) {
   $(document).ready(function ($) {
-    // News Slider
-    let gallery = jQuery('.gallery-carousel');
-    gallery.each(function () {
-      jQuery(this).owlCarousel({
-        nav: true,
-        navText: [
-          '<svg width="26" height="17" viewBox="0 0 26 17" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M25.3536 8.17676L0.353577 8.17676" stroke="#CDE0E7" stroke-width="0.5"/><path d="M8.35358 16.1768L0.353577 8.17676L8.35358 0.176757" stroke="#CDE0E7" stroke-width="0.5"/></svg>',
-          '<svg width="26" height="17" viewBox="0 0 26 17" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0 8.17676L25 8.17676" stroke="#CDE0E7" stroke-width="0.5"/><path d="M17 0.176758L25 8.17676L17 16.1768" stroke="#CDE0E7" stroke-width="0.5"/></svg>',
-        ],
-        navContainer: '.projects-nav',
-        loop: true,
-        dots: false,
-        margin: 20,
-        autoHeight: true,
-        responsive: {
-          0: {
-            items: 1,
-            autoWidth: false,
-          },
-          768: {
-            items: 4,
-            autoWidth: true,
-          },
-        },
-      });
-    });
     let timer = $('.countdown').attr('data-timer');
     function countdown(dateEnd) {
       var timer, days, hours, minutes, seconds;
@@ -83,5 +57,18 @@
     }
     // console.log('timer', timer);
     countdown(timer);
+
+    // Trigger Internal Section scroll event
+    $('a[href^="#"]').on('click', function (e) {
+      // prevent default anchor click behavior
+      e.preventDefault();
+
+      // store hash
+      var hash = this.hash;
+
+      if ($(hash).length) {
+        $('html, body').animate({ scrollTop: $(hash).offset().top - 100 }, 300);
+      }
+    });
   }); //document ready
 })(jQuery);
